3fbccca0d6306877cadf7bba969f9f174a88e848
Matthew Pace <pace4@purdue.edu>
2014-11-06 08:27:32 -0500

    Beginning of Bootstrap only restructuring

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 function loadVideo(videoEmbeddedUrl) {
   document.getElementById("content-container").innerHTML='<center><iframe width="960" height="720" src="http://' + videoEmbeddedUrl + '" frameborder="0" allowfullscreen></iframe></center>'
 }
+
+function insertForm(formName) {
+  
+}
+
