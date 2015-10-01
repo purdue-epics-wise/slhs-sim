0722a11469af49c520c922ebf09df304c54c5e05
Yatharth Khullar <yathukhullar@gmail.com>
2015-10-01 09:37:59 -0400

    age function to check for user input

--- age_func.js
+++ age_func.js
@@ -1,23 +1,25 @@
 function ageCheck(birth_day, birth_month, birth_year, test_day,  test_month, test_year, start_num)
-{
+{ 
+  //READ THROUGH FAQ PAGE FOR WEBSITE IF YOU DO NOT UNDERSTAND THIS FUNCTIONALITY
   //age is entered by user
   //start_num is where user thinks test should start
     
-  var start_calc;
+  var start_calc; //where test should start
   var month_diff = test_month - birth_month;
   var year_diff = test_year - birth_year;
   var day_diff = test_day - birth_month;
   
-  if(month_diff < 0)
+  if(month_diff < 0) //in case test taker has not completed a full year
   {
     year_diff -= 1;
   }
-  if(month_diff == 0 &&  day_diff < 0)
-  {
+  if(month_diff == 0 &&  day_diff < 0) 
+  { //in case test taker has not completed a full month and hence not full year
+  
     year_diff -= 1;
   }
   
-  switch(year_diff)
+  switch(year_diff) //calculates where test should start
   {
     case 4: 
 	start_calc = 13;
@@ -69,7 +71,7 @@ function ageCheck(birth_day, birth_month, birth_year, test_day,  test_month, tes
    }
 
    if(start_calc != start_num)
-   {
+   { //if user has entered wrong number to start at, error message to inform
      window.alert("wrong");
    }
 }
