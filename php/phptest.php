8b6c033c34a894ece2d0168ed13ad7427454e5bb
Samuel Yin <yin56@purdue.edu>
2016-11-30 19:29:33 -0500

    old changes

--- php/phptest.php
+++ php/phptest.php
@@ -2,10 +2,30 @@
 <html>
 <body>
 
-<h1>My first PHP page</h1>
+<h1>Connecting to SQL Database</h1>
 
 <?php
-echo "Hello World!";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "slhs"
+
+//$conn = new mysqli($servername, $username, $password);
+
+mysql_connect($servername,$username,$password) or die("Unable to connect to database");
+	//@sql_select_db($database) or die("Unable to select database");
+    //$conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+    // set the PDO error mode to exception
+	
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+echo "Connected successfully"; 
+
+
+msql_close($link);
 ?>
 
 </body>
