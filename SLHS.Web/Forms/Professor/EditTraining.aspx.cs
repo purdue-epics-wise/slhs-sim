5500306de22788a489f951bf310cb7550da124bd
pchang1996 <chang278@purdue.edu>
2018-01-10 20:23:33 -0500

    Add files via upload

--- SLHS.Web/Forms/Professor/EditTraining.aspx.cs
+++ SLHS.Web/Forms/Professor/EditTraining.aspx.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Diagnostics;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Configuration;
 
 namespace SLHS.Web.Forms.Professor
 {
@@ -11,9 +16,73 @@ namespace SLHS.Web.Forms.Professor
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-           
+
         }
 
-       
+        public int countRows()
+        {
+            string stmt = "SELECT COUNT(*) FROM dbo.Trains";
+            int count = 0;
+
+            using (SqlConnection thisConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["SLHS_DBConnectionString"].ConnectionString))
+            {
+                using (SqlCommand cmdCount = new SqlCommand(stmt, thisConnection))
+                {
+                    thisConnection.Open();
+                    count = (int)cmdCount.ExecuteScalar();
+                }
+            }
+            return count;
+        }
+
+        protected void convertToBinaryStream()
+        {
+            using (BinaryReader br = new BinaryReader(FileUploadControl.PostedFile.InputStream))
+            {
+                byte[] bytes = br.ReadBytes((int)FileUploadControl.PostedFile.InputStream.Length);
+                string strConnString = ConfigurationManager.ConnectionStrings["SLHS_DBConnectionString"].ConnectionString;
+                int count = countRows();
+                using (SqlConnection con = new SqlConnection(strConnString))
+                {
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+                        cmd.CommandText = "insert into Trains(Name, Data) values (@Name, @Data)";
+                        cmd.Parameters.AddWithValue("@Name", "Training " + (count + 1).ToString());
+                        cmd.Parameters.AddWithValue("@Data", bytes);
+                        cmd.Connection = con;
+                        con.Open();
+                        cmd.ExecuteNonQuery();
+                        con.Close();
+                    }
+                }
+            }
+        }
+
+        protected void UploadButton_Click(object sender, EventArgs e)
+        {
+            if (FileUploadControl.HasFile)
+            {
+                try
+                {
+                    string filename = Path.GetFileName(FileUploadControl.FileName);
+                    if(Path.GetExtension(filename) == ".webm")
+                    {
+                        convertToBinaryStream();
+                        
+                        StatusLabel.Text = "Upload status: File uploaded!";
+                    }
+                    else
+                    {
+                        StatusLabel.Text = "Upload status: The file could not be uploaded. The following error occured: File needs to be in .webm format";                       
+                    }
+                    FileUploadControl.SaveAs(Server.MapPath("~/") + filename);
+                }
+                catch (Exception ex)
+                {
+                    StatusLabel.Text = "Upload status: The file could not be uploaded. The following error occured: " + ex.Message;
+                }
+            }
+        }
+
     }
 }
--- SLHS.Web/Forms/Professor/EditTraining.aspx.designer.cs
+++ SLHS.Web/Forms/Professor/EditTraining.aspx.designer.cs
@@ -11,5 +11,32 @@ namespace SLHS.Web.Forms.Professor {
     
     
     public partial class EditTraining {
+        
+        /// <summary>
+        /// FileUploadControl control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.FileUpload FileUploadControl;
+        
+        /// <summary>
+        /// UploadButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button UploadButton;
+        
+        /// <summary>
+        /// StatusLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label StatusLabel;
     }
 }
