9938477a655f498341574bb7d6847d43499149c7
kozlows1 <31451653+kozlows1@users.noreply.github.com>
2018-02-27 14:40:06 -0500

    video upload fixed

--- SLHS.Web/Forms/Professor/EditTraining.aspx.cs
+++ SLHS.Web/Forms/Professor/EditTraining.aspx.cs
@@ -35,29 +35,31 @@ namespace SLHS.Web.Forms.Professor
             return count;
         }
 
-        protected void convertToBinaryStream()
-        {
-            using (BinaryReader br = new BinaryReader(FileUploadControl.PostedFile.InputStream))
-            {
-                byte[] bytes = br.ReadBytes((int)FileUploadControl.PostedFile.InputStream.Length);
-                string strConnString = ConfigurationManager.ConnectionStrings["SLHS_DBConnectionString"].ConnectionString;
-                int count = countRows();
-                using (SqlConnection con = new SqlConnection(strConnString))
-                {
-                    using (SqlCommand cmd = new SqlCommand())
-                    {
-                        cmd.CommandText = "insert into Trains(Name, Data) values (@Name, @Data)";
-                        cmd.Parameters.AddWithValue("@Name", "Training" + (count + 1).ToString());
-                        cmd.Parameters.AddWithValue("@Data", bytes);
-                        cmd.Connection = con;
-                        con.Open();
-                        cmd.ExecuteNonQuery();
-                        con.Close();
-                    }
-                }
-            }
-        }
+       protected void ConvertToBinaryStream()
+       {
+           using (BinaryReader br = new BinaryReader(FileUploadControl.PostedFile.InputStream))
+          {
+               byte[] bytes = br.ReadBytes((int)FileUploadControl.PostedFile.InputStream.Length);
+               string strConnString = ConfigurationManager.ConnectionStrings["SLHS_DBConnectionString"].ConnectionString;
+               int count = countRows();
+               using (SqlConnection con = new SqlConnection(strConnString))
+               {
+                   using (SqlCommand cmd = new SqlCommand())
+                   {
+                       cmd.CommandText = "insert into Trains(Name, Data) values (@Name, @Data)";  
+                       cmd.Parameters.AddWithValue("@Name", "Training " + (count + 1).ToString());
+                       cmd.Parameters.AddWithValue("@Data", bytes);
+                       cmd.Connection = con;
+                       con.Open();
+                       cmd.ExecuteNonQuery();
+                       con.Close();
+                   }
+               }
+           }
+       }
 
+ 
+        //TODO: Make it possible to upload files
         protected void UploadButton_Click(object sender, EventArgs e)
         {
             if (FileUploadControl.HasFile)
@@ -65,20 +67,27 @@ namespace SLHS.Web.Forms.Professor
                 try
                 {
                     string filename = Path.GetFileName(FileUploadControl.FileName);
-                    if(Path.GetExtension(filename) == ".webm")
+                    if(Path.GetExtension(filename) == ".webm") //checks to see if the video is in the correct format
                     {
-                        convertToBinaryStream();
+                        //ensures video is saved first before data is stripped by ConvertToBinaryStream()
+                        FileUploadControl.SaveAs(Server.MapPath("/assets/video/") + filename); 
+
+                        //uploads video data to database, iterates what training number the video belongs to
+                        //this deletes the data from the video so it must be done AFTER video is properly saved
+                        ConvertToBinaryStream();
                         
                         StatusLabel.Text = "Upload status: File uploaded!";
                     }
                     else
                     {
-                        StatusLabel.Text = "Upload status: The file could not be uploaded. The following error occured: File needs to be in .webm format";                       
+                        //refuses upload if format is incorrect
+                        StatusLabel.Text = "Upload status: The file could not be uploaded. The following error occured: File needs to be in .webm format";                         
                     }
-                    FileUploadControl.SaveAs(Server.MapPath("~/") + filename);
+                    
                 }
                 catch (Exception ex)
                 {
+                    //refuses upload for other problems
                     StatusLabel.Text = "Upload status: The file could not be uploaded. The following error occured: " + ex.Message;
                 }
             }
